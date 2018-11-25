9239561777bf3286db889e450956431dd08c447e
evaganov <evaganov@guidewire.com>
2018-11-25 02:03:12 -0800

    sr-carrier-backend changes: - test commit;

--- sr-carrier-backend/src/main/java/com/guidewire/devconnect/EntryPoint.java
+++ sr-carrier-backend/src/main/java/com/guidewire/devconnect/EntryPoint.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 public class EntryPoint {
   private static Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+
   public static void main(String[] args) throws InterruptedException {
     LOGGER.info("Starting EntryPoint");
 
