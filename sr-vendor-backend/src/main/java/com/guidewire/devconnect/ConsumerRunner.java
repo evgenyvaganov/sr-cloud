82c5962a3aaf93f5c70d1cdbf163240763f1f432
evaganov <evaganov@guidewire.com>
2018-11-25 01:36:15 -0800

    - added Dockerfile; - refactored to a single entry point; - removed direct usages for System.out and changed them to LOGGER; - added fatJar gradle task;

---
+++ sr-vendor-backend/src/main/java/com/guidewire/devconnect/ConsumerRunner.java
@@ -0,0 +1,41 @@
+package com.guidewire.devconnect;
+
+import java.lang.invoke.MethodHandles;
+import java.time.Duration;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerRunner implements Runnable {
+  private static Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  public void run() {
+    LOGGER.info("Consumer started");
+
+    Consumer<Long, String> consumer = ConsumerCreator.createConsumer();
+
+    try {
+      while (true) {
+        ConsumerRecords<Long, String> consumerRecords = consumer.poll(Duration.ofMillis(5000));
+        // 1000 is the time in milliseconds consumer will wait if no record is found at broker.
+        if (consumerRecords.count() == 0) {
+          LOGGER.info("No data");
+          continue;
+        }
+        //print each record.
+        consumerRecords.forEach(record -> {
+          LOGGER.info("Record Key {}", record.key());
+          LOGGER.info("Record value {}", record.value());
+          LOGGER.info("Record partition {}", record.partition());
+          LOGGER.info("Record offset {}", record.offset());
+        });
+        // commits the offset of record to broker.
+        consumer.commitAsync();
+      }
+    } catch (Exception e) {
+      LOGGER.info("Consumer finished (interrupted)", e);
+    }
+  }
+}
--- sr-vendor-backend/src/main/java/com/guidewire/devconnect/ProducerRunner.java
+++ sr-vendor-backend/src/main/java/com/guidewire/devconnect/ProducerRunner.java
@@ -1,5 +1,6 @@
 package com.guidewire.devconnect;
 
+import java.lang.invoke.MethodHandles;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -7,20 +8,26 @@ import java.util.concurrent.TimeoutException;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ProducerMain {
-  public static void main(String[] args) {
+public class ProducerRunner implements Runnable {
+  private static Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  public void run() {
+    LOGGER.info("Producer started");
+
     Producer<Long, String> producer = ProducerCreator.createProducer();
     for (int index = 0; index < 1000; ++index) {
       ProducerRecord<Long, String> record = new ProducerRecord<>("vendor-out", "This is record " + index);
       try {
         RecordMetadata metadata = producer.send(record).get(1000, TimeUnit.SECONDS);
-        System.out.println("Record sent with key " + index + " to partition " + metadata.partition()
-          + " with offset " + metadata.offset());
+        LOGGER.info("Record sent with key {} to partition {} with offset {}", index, metadata.partition(), metadata.offset());
       } catch (ExecutionException | InterruptedException | TimeoutException e) {
-        System.out.println("Error in sending record");
-        System.out.println(e);
+        LOGGER.info("Error in sending record", e);
       }
     }
+
+    LOGGER.info("Producer finished");
   }
 }
